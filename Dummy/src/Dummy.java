5058735882d21552de35d4e001efad23ccb00e4f
praful-meshram <praful@giantleapsystems.com>
2014-03-21 15:02:39 +0530

    Update Dummy.java

--- Dummy/src/Dummy.java
+++ Dummy/src/Dummy.java
@@ -1,11 +1,10 @@
-
 public class Dummy {
 	public static void main(String[] args) {
 		System.out.println("dummy ");
 	}
 	public void name() {
 		System.out.println("nae szhn");
-		System.out.println("hjghjknzj");
+	
 		
 	}
 }
