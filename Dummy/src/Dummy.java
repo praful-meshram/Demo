0cd01680eeee37d825142a2e0b519846d69b612c
praful <praful@giantleapsystems.com>
2014-03-21 15:14:16 +0530

    faGb

--- Dummy/src/Dummy.java
+++ Dummy/src/Dummy.java
@@ -2,7 +2,7 @@ public class Dummy {
 	public static void main(String[] args) {
 		System.out.println("dummy ");
 	}
-	public void getname() {
+	public void name() {
 		System.out.println("nae szhn");
 	
 		
